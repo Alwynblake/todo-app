8d7b8c555d74f04844230d1a3c9d118a97e8a1e1
Alistair Blake <consultations@me.com>
2019-07-06 01:37:07 -0700

    made changes to landing

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -5,7 +5,8 @@ const Landing = () => (
     <h2>This simple todo application is a task-management tool.
       It allows users to manage tasks from a computer or smartphone.
       This application contains a fixed side navigation menu,
-      and the use of SCSS to create an animated modal (dialog box) that is hidden by default.
+      the use of SCSS to create a playful layered paper effect, and
+      an animated modal (dialog box) that is hidden by default.
       The implementation of React JS, when combined with a renderer,
       captures data and responds to user inputs.</h2>
 );
